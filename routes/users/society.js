be861eb6ae585412f586decef1dcfce5a02c6853
JamesYin <elantion@163.com>
2015-08-24 17:13:01 +0800

    update

--- routes/users/society.js
+++ routes/users/society.js
@@ -3,7 +3,8 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/weibo/register', function (req, res, next) {
-
+    res.header("Access-Control-Allow-Origin", "https://api.weibo.com");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 });
 
 module .exports = router;
